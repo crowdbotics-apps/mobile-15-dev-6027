43a603ad5a8ec26f0a540dcd389bb1aa4a6be897
Crowdbotics <team@crowdbotics.com>
2020-06-15 07:44:01 +0000

    add package blueprint-react-native-calendar-screen

--- src/redux/mainReducer.js
+++ src/redux/mainReducer.js
@@ -6,6 +6,7 @@ import { combineReducers } from "redux";
 
 
 //@BlueprintReduxImportInsertion
+import CalendarView5651Reducer from '../features/CalendarView5651/redux/reducers';
 import CalendarReducer from '../features/Calendar/redux/reducers';
 import EmailAuthReducer from '../features/EmailAuth/redux/reducers';
 
@@ -17,6 +18,7 @@ export const combinedReducers = combineReducers({
 
 
   //@BlueprintReduxCombineInsertion
+CalendarView5651: CalendarView5651Reducer,
 Calendar: CalendarReducer,
 EmailAuth: EmailAuthReducer,
 
